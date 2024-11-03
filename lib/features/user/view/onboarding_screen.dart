abed2a7bf7f49bc2d23d783ed6b79890ff7ce3e1
hin6150 <hin6150@gmail.com>
2024-11-03 19:15:03 +0900

    feat: 온보딩 UIUX 개선

--- lib/features/user/view/onboarding_screen.dart
+++ lib/features/user/view/onboarding_screen.dart
@@ -1,6 +1,5 @@
-import 'package:dorun_app_flutter/common/component/custom_button.dart';
-import 'package:dorun_app_flutter/common/component/gap_row.dart';
 import 'package:dorun_app_flutter/common/constant/colors.dart';
+import 'package:dorun_app_flutter/common/constant/fonts.dart';
 import 'package:dorun_app_flutter/features/user/view/components/chat_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -18,16 +17,12 @@ class OnboardingScreen extends StatefulWidget {
 class OnboardingScreenState extends State<OnboardingScreen> {
   late int _currentIndex;
   bool _isLoading = false;
-  bool _isQuestion = false;
 
   final List<String> messages = [
-    "안녕하세요! 저와 대화하려면 저를 클릭해주세요!",
-    "반가워요! 저는 루틴 설계를 도와주는 루틴 디자이너, 라디에요!",
-    "앞으로 사용자님이 루틴을 통해 원하시는 일들을 이룰 수 있도록 돕는 업무를 맡고 있어요!",
-    "혹시... 루틴에 대해서 알고 계신가요?",
-    "루틴은 일정한 규칙과 순서에 따라 반복적으로 수행되는 활동이나 절차를 의미해요.",
-    "언뜻 보면 습관과 비슷해보이지만 무의식적으로 행동을 수행하는 습관과 다르게 루틴은 의식적으로 반복하기 위해 세운 계획에 더 가까워요.",
-    "좋아요 ~! 이제 저와 함께 루틴을 통해 사용자님의 삶을 디자인하러 가봐요!",
+    "안녕하세요!\n저와 대화하려면 저를 클릭해주세요!",
+    "반가워요!\n저는 사용자의 삶을 설계해주는 라이프 디자이너, 라디에요!",
+    "살아가면서 한가지 목표를 이룰 수 있으면 무엇을 하고 싶나요?",
+    "저와 함께 사용자님이 진짜 원하는 것을 찾아서 이를 습관으로 만들어봐요!"
   ];
 
   int _loadingIndex = 0;
@@ -56,14 +51,14 @@ class OnboardingScreenState extends State<OnboardingScreen> {
     });
 
     for (int i = 0; i < 3; i++) {
-      await Future.delayed(const Duration(milliseconds: 500), () {
+      await Future.delayed(const Duration(milliseconds: 300), () {
         setState(() {
           _loadingIndex = i + 1;
         });
       });
     }
 
-    await Future.delayed(const Duration(milliseconds: 500), () {
+    await Future.delayed(const Duration(milliseconds: 300), () {
       setState(() {
         _isLoading = false;
       });
@@ -71,23 +66,23 @@ class OnboardingScreenState extends State<OnboardingScreen> {
   }
 
   Future<void> _nextMessage() async {
-    if (_isQuestion) return;
-
     if (_currentIndex < messages.length - 1) {
-      if (_currentIndex == 2) {
-        if (!mounted) return;
-        setState(() {
-          _currentIndex++;
-          _startLoading();
-        });
-        await Future.delayed(const Duration(milliseconds: 2000), () {
-          if (!mounted) return;
-          setState(() {
-            _isQuestion = true;
-          });
-        });
-        return;
-      }
+      // if (_currentIndex == 2) {
+      //   if (!mounted) return;
+      //   setState(() {
+      //     _currentIndex++;
+      //     _startLoading();
+      //   });
+
+      //   await Future.delayed(const Duration(milliseconds: 2000), () {
+      //     if (!mounted) return;
+      //     setState(() {
+      //       _isQuestion = true;
+      //     });
+      //   });
+      //   return;
+      // }
+
       if (!mounted) return;
       setState(() {
         _currentIndex++;
@@ -137,61 +132,41 @@ class OnboardingScreenState extends State<OnboardingScreen> {
                             fit: BoxFit.contain,
                             width: MediaQuery.of(context).size.width * 0.7,
                           )
-                        : _currentIndex == 6
-                            ? Image.asset(
-                                'asset/images/congrate.gif',
-                                fit: BoxFit.contain,
-                                width: MediaQuery.of(context).size.width * 0.7,
-                              )
-                            : Image.asset(
-                                'asset/images/working.gif',
-                                fit: BoxFit.contain,
-                                width: MediaQuery.of(context).size.width * 0.7,
-                              ),
+                        : Image.asset(
+                            'asset/images/working.gif',
+                            fit: BoxFit.contain,
+                            width: MediaQuery.of(context).size.width * 0.7,
+                          ),
                   ),
                   const SizedBox(height: 48),
                 ],
               ),
             ),
-            Container(
-              height: 103,
-              padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
-              child: _isQuestion
-                  ? GapRow(
-                      gap: 16,
-                      children: [
-                        Expanded(
-                          child: CustomButton(
-                            backgroundColor: AppColors.BRAND_SUB,
-                            foregroundColor: AppColors.BRAND,
-                            title: '모른다',
-                            onPressed: () {
-                              setState(() {
-                                _currentIndex++;
-                                _isQuestion = false;
-                                _startLoading();
-                              });
-                            },
-                          ),
-                        ),
-                        Expanded(
-                          child: CustomButton(
-                            backgroundColor: AppColors.BRAND,
-                            foregroundColor: Colors.white,
-                            title: '알고있다',
-                            onPressed: () {
-                              setState(() {
-                                _currentIndex = 6;
-                                _isQuestion = false;
-                                _startLoading();
-                              });
-                            },
-                          ),
-                        ),
-                      ],
-                    )
-                  : null,
-            )
+            SizedBox(
+              width: double.infinity,
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: const EdgeInsetsDirectional.symmetric(
+                    horizontal: 32,
+                    vertical: 40,
+                  ),
+                  child: GestureDetector(
+                    onTap: () {
+                      context.go('/userInfo');
+                    },
+                    child: Text(
+                      '건너뛰기',
+                      textAlign: TextAlign.end,
+                      style: AppTextStyles.MEDIUM_16.copyWith(
+                        color: AppColors.TEXT_SECONDARY,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
--- lib/features/user/view/user_info_screen.dart
+++ lib/features/user/view/user_info_screen.dart
@@ -1,12 +1,15 @@
 import 'package:dorun_app_flutter/common/component/custom_bottom_sheet.dart';
 import 'package:dorun_app_flutter/common/component/custom_button.dart';
 import 'package:dorun_app_flutter/common/component/gap_column.dart';
+import 'package:dorun_app_flutter/common/component/gap_row.dart';
 import 'package:dorun_app_flutter/common/component/input_box.dart';
 import 'package:dorun_app_flutter/common/constant/colors.dart';
 import 'package:dorun_app_flutter/common/constant/fonts.dart';
 import 'package:dorun_app_flutter/common/layout/default_layout.dart';
+import 'package:dorun_app_flutter/features/routine/view/components/repeat_button.dart';
 import 'package:dorun_app_flutter/features/user/provider/user_me_provider.dart';
 import 'package:dorun_app_flutter/features/user/repository/user_repository.dart';
+import 'package:dorun_app_flutter/features/user/view/components/chat_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -21,18 +24,22 @@ class UserInfoScreen extends ConsumerStatefulWidget {
 }
 
 class _UserInfoScreenState extends ConsumerState<UserInfoScreen> {
+  bool isFinished = false;
+
   final TextEditingController _nameController = TextEditingController();
   String? _selectedName;
   String? _selectedAge;
   String? _selectedGender;
   String? _selectedJob;
-  List<String> _selectedDifficulties = [];
   List<String> guideQuestions = [
     '그전에 혹시\n사용자님을 어떻게 부르면 될까요?',
-    '님께 더 나은 루틴을 제공하기 위해 나이정보가 필요해요.',
-    '성별을 알려주세요',
-    '하시는 일을 알려주세요',
+    '님을 보다 잘 이해하기 위해서 나이 정보가 필요해요.',
+    '성별은 어떻게 되나요?',
+    '하시는 일이 무엇인가요?',
   ];
+
+  List<String> jobOptions = ["학생", "직장인", "프리랜서", "자영업자", "취준생", "기타"];
+  List<String> genderOptions = ["남성", "여성"];
 
   String getCurrentGuideQuestion() {
     if (_selectedName == null) {
@@ -42,10 +49,8 @@ class _UserInfoScreenState extends ConsumerState<UserInfoScreen> {
       return '사용자${guideQuestions[1]}';
     } else if (_selectedGender == null) {
       return guideQuestions[2];
-    } else if (_selectedJob == null) {
-      return guideQuestions[3];
     } else {
-      return "계획을 지키면서 어려운점이 있으신가요?";
+      return guideQuestions[3];
     }
   }
 
@@ -70,172 +75,241 @@ class _UserInfoScreenState extends ConsumerState<UserInfoScreen> {
       backgroundColor: Colors.white,
       child: Padding(
         padding: const EdgeInsets.all(24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: GapColumn(
-                  gap: 16,
-                  children: [
-                    Text(
-                      getCurrentGuideQuestion(),
-                      style: AppTextStyles.BOLD_20,
+        child: isFinished
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 32,
+                        horizontal: 32,
+                      ),
+                      child: const Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          ChatBubble(
+                              text: "좋아요~! 거의 다 왔어요.\n이제 저와 함께 습관을 만들러 가봐요"),
+                        ],
+                      ),
                     ),
-                    if (_selectedName != null &&
-                        _selectedAge != null &&
-                        _selectedGender != null &&
-                        _selectedJob != null)
-                      Builder(builder: (innerContext) {
-                        return ReadOnlyBox(
-                          hintText: "계획 중 평소 어려워하는 내용",
-                          inputText: _selectedDifficulties.join(', '),
-                          onTap: () {
-                            showSelectionMulitySheet(innerContext, [
-                              "목표 불명확",
-                              "꾸준함 부족",
-                              "과도한 이상화",
-                              "일정 변화",
-                              "동기 저하",
-                              "시간 부족",
-                              "실패 두려움",
-                              "완벽주의",
-                              "즉각적인 결과 욕구",
-                              "자책감"
-                            ], (selectedValues) {
-                              setState(() {
-                                _selectedDifficulties = selectedValues;
-                              });
-                            }, _selectedDifficulties);
-                          },
-                        );
-                      }),
-                    if (_selectedName != null &&
-                        _selectedAge != null &&
-                        _selectedGender != null)
-                      Builder(builder: (innerContext) {
-                        return ReadOnlyBox(
-                          hintText: "직업",
-                          inputText: _selectedJob ?? '',
-                          onTap: () {
-                            showSelectionSheet(
-                              innerContext,
-                              ["학생", "직장인", "프리랜서", "자영업자", "취준생", "기타"],
-                              (value) {
-                                setState(() {
-                                  _selectedJob = value;
-                                });
+                  ),
+                  Center(
+                    child: Image.asset(
+                      'asset/images/congrate.gif',
+                      fit: BoxFit.contain,
+                      width: MediaQuery.of(context).size.width * 0.7,
+                    ),
+                  ),
+                  const SizedBox(height: 48),
+                  Container(
+                    height: 103,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 24,
+                    ),
+                    child: GapRow(
+                      gap: 16,
+                      children: [
+                        Expanded(
+                          child: CustomButton(
+                            backgroundColor: AppColors.BRAND_SUB,
+                            foregroundColor: AppColors.BRAND,
+                            title: '나중에 할게요',
+                            onPressed: () async {
+                              await userRepository.updateUser(
+                                name: _selectedName,
+                                age: _selectedAge,
+                                job: _selectedJob,
+                                gender: _selectedGender,
+                                memberStatus: 'Onboarding',
+                              );
+                              if (context.mounted) context.go('/');
+                              ref.invalidate(userMeProvider);
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: CustomButton(
+                            backgroundColor: AppColors.BRAND,
+                            foregroundColor: Colors.white,
+                            title: '좋아요',
+                            onPressed: () async {
+                              await userRepository.updateUser(
+                                name: _selectedName,
+                                age: _selectedAge,
+                                job: _selectedJob,
+                                gender: _selectedGender,
+                                memberStatus: 'Onboarding',
+                              );
+                              if (context.mounted) {
+                                context.go('/');
+                                context.push('/routine_create');
+                              }
+                              ref.invalidate(userMeProvider);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: GapColumn(
+                        gap: 16,
+                        children: [
+                          Text(
+                            getCurrentGuideQuestion(),
+                            style: AppTextStyles.BOLD_20,
+                          ),
+                          if (_selectedName != null &&
+                              _selectedAge != null &&
+                              _selectedGender != null)
+                            Builder(builder: (innerContext) {
+                              List<Widget> gapRows = List.generate(
+                                3,
+                                (rowIndex) => GapRow(
+                                  gap: 16,
+                                  children: List.generate(
+                                    2,
+                                    (colIndex) {
+                                      int index = rowIndex * 2 + colIndex;
+                                      String job = jobOptions[index];
+
+                                      return Expanded(
+                                        child: buildRepeatOptionButton(
+                                          job,
+                                          _selectedJob == job,
+                                          () {
+                                            setState(() {
+                                              _selectedJob = job;
+                                            });
+                                          },
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                              );
+                              return GapColumn(gap: 16, children: [
+                                const Text(
+                                  "직업",
+                                  style: AppTextStyles.MEDIUM_12,
+                                ),
+                                ...gapRows
+                              ]);
+                            }),
+                          if (_selectedName != null && _selectedAge != null)
+                            Builder(
+                              builder: (innerContext) {
+                                return GapColumn(
+                                  gap: 16,
+                                  children: [
+                                    const Text("성별",
+                                        style: AppTextStyles.MEDIUM_12),
+                                    GapRow(
+                                      gap: 16,
+                                      children: [
+                                        Expanded(
+                                          child: buildRepeatOptionButton(
+                                              genderOptions[0],
+                                              _selectedGender ==
+                                                  genderOptions[0], () {
+                                            setState(() {
+                                              _selectedGender =
+                                                  genderOptions[0];
+                                            });
+                                          }),
+                                        ),
+                                        Expanded(
+                                          child: buildRepeatOptionButton(
+                                              genderOptions[1],
+                                              _selectedGender ==
+                                                  genderOptions[1], () {
+                                            setState(() {
+                                              _selectedGender =
+                                                  genderOptions[1];
+                                            });
+                                          }),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                );
                               },
-                              "하시는 일이 무엇인가요?",
-                              _selectedJob ?? '',
-                            );
-                          },
-                        );
-                      }),
-                    if (_selectedName != null && _selectedAge != null)
-                      Builder(builder: (innerContext) {
-                        return ReadOnlyBox(
-                          hintText: "성별",
-                          inputText: _selectedGender ?? '',
-                          onTap: () {
-                            showSelectionSheet(
-                              innerContext,
-                              ["남성", "여성"],
-                              (value) {
-                                setState(() {
-                                  _selectedGender = value;
-                                });
-                              },
-                              "성별을 알려주세요",
-                              _selectedGender ?? '',
-                            );
-                          },
-                        );
-                      }),
-                    if (_selectedName != null)
-                      Builder(builder: (innerContext) {
-                        return ReadOnlyBox(
-                          hintText: "나이",
-                          inputText: _selectedAge != null && _selectedAge != ''
-                              ? '$_selectedAge살'
-                              : '',
-                          onTap: () {
-                            showYearPicker(
-                              innerContext,
-                              (value) {
+                            ),
+                          if (_selectedName != null)
+                            Builder(builder: (innerContext) {
+                              return ReadOnlyBox(
+                                hintText: "나이",
+                                inputText:
+                                    _selectedAge != null && _selectedAge != ''
+                                        ? '$_selectedAge살'
+                                        : '',
+                                onTap: () {
+                                  showYearPicker(
+                                    innerContext,
+                                    (value) {
+                                      setState(() {
+                                        _selectedAge =
+                                            (DateTime.now().year - value)
+                                                .toString();
+                                      });
+                                    },
+                                  );
+                                },
+                              );
+                            }),
+                          InputBox(
+                            controller: _nameController,
+                            hintText: '이름',
+                            onSubmitted: _setUserName,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  if (_selectedAge != null &&
+                      _selectedGender != null &&
+                      _selectedName != null &&
+                      _selectedJob != null)
+                    CustomButton(
+                      backgroundColor: AppColors.BRAND,
+                      foregroundColor: Colors.white,
+                      title: "완료하기",
+                      onPressed: () async {
+                        setState(() {
+                          isFinished = true;
+                        });
+                      },
+                    ),
+                  if (_selectedAge == null)
+                    Builder(builder: (innerContext) {
+                      return CustomButton(
+                        title: "나중에 입력할래요",
+                        onPressed: () {
+                          setState(() {
+                            _selectedName ??= '';
+
+                            if (_selectedAge == null) {
+                              showYearPicker(context, (value) {
                                 setState(() {
                                   _selectedAge =
                                       (DateTime.now().year - value).toString();
                                 });
-                              },
-                            );
-                          },
-                        );
-                      }),
-                    InputBox(
-                      controller: _nameController,
-                      hintText: '이름',
-                      onSubmitted: _setUserName,
-                    ),
-                  ],
-                ),
+                              });
+                            }
+                          });
+                        },
+                      );
+                    }),
+                ],
               ),
-            ),
-            if (_selectedDifficulties.isEmpty)
-              Builder(builder: (innerContext) {
-                return CustomButton(
-                  title: "나중에 입력할래요",
-                  onPressed: () {
-                    setState(() {
-                      _selectedName ??= '';
-                      _selectedAge ??= '';
-                      _selectedGender ??= '';
-                      _selectedJob ??= '';
-
-                      showSelectionMulitySheet(innerContext, [
-                        "목표 불명확",
-                        "꾸준함 부족",
-                        "과도한 이상화",
-                        "일정 변화",
-                        "동기 저하",
-                        "시간 부족",
-                        "실패 두려움",
-                        "완벽주의",
-                        "즉각적인 결과 욕구",
-                        "자책감",
-                      ], (selectedValues) {
-                        setState(() {
-                          _selectedDifficulties = selectedValues;
-                        });
-                      }, _selectedDifficulties);
-                    });
-                  },
-                );
-              }),
-            if (_selectedAge != null &&
-                _selectedDifficulties.isNotEmpty &&
-                _selectedGender != null &&
-                _selectedName != null &&
-                _selectedJob != null)
-              CustomButton(
-                backgroundColor: AppColors.BRAND,
-                foregroundColor: Colors.white,
-                title: "완료하기",
-                onPressed: () async {
-                  await userRepository.updateUser(
-                    name: _selectedName,
-                    age: _selectedAge,
-                    job: _selectedJob,
-                    gender: _selectedGender,
-                    challenges: _selectedDifficulties,
-                    memberStatus: 'Onboarding',
-                  );
-                  if (context.mounted) context.go('/');
-                  ref.invalidate(userMeProvider);
-                },
-              ),
-          ],
-        ),
       ),
     );
   }
